c43e909b057ed389cf2626479f6ffbd7721f4538
antony-chi <antony.cp9@gmail.com>
2023-05-27 22:45:53 -0500

    se agrego router.post en router para preparar el objeto que llega del html

--- src/app.js
+++ src/app.js
@@ -1,29 +1,34 @@
 import express from "express";
-import {create} from 'express-handlebars'; //se cambio por import exphbs from "express-handlebar"
-import indexRoutes from './routes/index.routes';
-import path from 'path';
+import { create } from "express-handlebars"; //se cambio por import exphbs from "express-handlebar"
+import indexRoutes from "./routes/index.routes";
+import path from "path";
+import morgan from "morgan";
 
 //aplicacion de servidor
 const app = express();
 
-//---configuracion de motor de plantilla
-app.set('views', path.join(__dirname, 'views'));
+//------configuracion de motor de plantilla
+app.set("views", path.join(__dirname, "views"));
 
-const hbs = create({//se creo la constante HBS = create
-    layoutsDir: path.join(app.get('views'),'layouts'),//indicamos donde se encuentra la carpeta layouts
-    partialsDir: path.join(app.get('views'),'partials'),//indicamos como se llama la carpteta PARCIALS cuando cambiamos el nombre
-    defaultLayout: 'main',
-    extname:".hbs"
+const hbs = create({
+  //se creo la constante HBS = create
+  layoutsDir: path.join(app.get("views"), "layouts"), //indicamos donde se encuentra la carpeta layouts
+  partialsDir: path.join(app.get("views"), "partials"), //indicamos como se llama la carpteta PARCIALS cuando cambiamos el nombre
+  defaultLayout: "main",
+  extname: ".hbs",
 });
 //aqui metimos la constante creada HBS en el argumento .engine
-app.engine('.hbs',hbs.engine)
+app.engine(".hbs", hbs.engine);
 //_________configuracion de motor de platilla ______
 
 //usamos la confihuracion
-app.set('view engine','.hbs')
+app.set("view engine", ".hbs");
 
+//middlewares
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: false}))
 //Routes
-app.use(indexRoutes)
-
+app.use(indexRoutes);
 
 export default app;
